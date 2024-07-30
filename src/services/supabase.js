48b5688f5140a699245d75f20fb36cc7007fde09
Aleksandra-Destro <aleksandra.destro@hotmail.com>
2024-07-30 09:11:49 +0200

    on_deploying

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://hmanfgstzdlrwfaakfrc.supabase.co";
+export const supabaseUrl = "https://hmanfgstzdlrwfaakfrc.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYW5mZ3N0emRscndmYWFrZnJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3OTIyNjYsImV4cCI6MjAzMjM2ODI2Nn0.o9y33k0KwJM3c8ySuwFyR70WoJLJz3f1xbZgoHQLXS4";
 const supabase = createClient(supabaseUrl, supabaseKey);
